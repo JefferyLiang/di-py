d3c4555c90eef52d70c786668c8b3da0f97cebfd
Jeffery Liang <jefferyliang@JefferydeMacBook-Pro.local>
2020-12-18 14:47:25 +0800

    [BUG FIX] 修复依赖注入时候注入参数的模式

--- lib/di/di_instance_create.py
+++ lib/di/di_instance_create.py
@@ -1,3 +1,6 @@
+import re
+
+
 class DIInstanceCreate:
     def __init__(self, cls, inject_kwargs, cache: bool = False):
         self._cls = cls
@@ -10,9 +13,13 @@ class DIInstanceCreate:
         if 'di_context' in kwargs:
             di_context = kwargs['di_context']
             for key in self._inject_kwargs:
-                svc = getattr(di_context, key, None)
+                regex = re.compile('[A-Z][a-z]*')
+                inject_name = str.lower('_'.join([match.group() for match in regex.finditer(key)]))
+                svc = di_context.get_instance(key)
                 if svc is None:
                     svc = getattr(di_context.app, key, None)
-                inject_dict[key] = svc
-        del kwargs['di_context']
+                    if svc is None:
+                        raise KeyError()
+                inject_dict.update({inject_name: svc})
+            del kwargs['di_context']
         return self._cls(**inject_dict, **kwargs)
--- lib/di/di_repository.py
+++ lib/di/di_repository.py
@@ -1,5 +1,6 @@
 from lib.di.di import DIContext
 from lib.di.di_injector import DIInjector
+from lib.di.di_instance_create import DIInstanceCreate
 
 
 class DIRepository:
@@ -24,13 +25,13 @@ class DIRepository:
             raise KeyError()
         return repository
 
-    def get_instance(self, name, cache: bool = False):
+    def get_instance(self, name):
         instance = self._instance_dict.get(name, None)
         if instance is None:
             cls = self._repos_dict.get(name, None)
             if cls is None:
                 raise KeyError()
             instance = self._injector.get_inject_instance(cls)
-            if cache:
+            if instance(cls, DIInstanceCreate) and getattr(cls, '_cache', False):
                 self._instance_dict[name] = instance
         return instance
